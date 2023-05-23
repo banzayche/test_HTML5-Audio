28845bd5b7e892f4d94493e39b530fcb9426dbb0
banzayche <dharm.serg@gmail.com>
2023-05-23 23:18:33 +0300

    Reconfigured webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,43 @@
-const path = require('path');
-
+const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
+const path = require( 'path' );
 module.exports = {
+  context: __dirname,
   entry: './src/index.js',
-  mode: 'development',
-  devServer: {
-    static: {
-      directory: path.join(__dirname, ''),
-    },
-    compress: true,
-    port: 9000,
-  },
   output: {
-    path: path.resolve(__dirname, '/dist'),
-    filename: 'bundle.js',
+    path: path.resolve( __dirname, 'dist' ),
+    filename: 'main.js',
+    publicPath: '/',
+  },
+  devServer: {
+    historyApiFallback: true
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
         use: 'babel-loader',
       },
-    ],
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|j?g|svg|gif)?$/,
+        use: 'file-loader'
+      }
+    ]
   },
-  resolve: {
-    extensions: ['.js'],
-  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve( __dirname, 'public/index.html' ),
+      filename: 'index.html'
+    })
+  ]
 };
