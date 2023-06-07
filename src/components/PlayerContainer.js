263bc9ea2eff583406c3b8e5cc561c8abd3f81d4
banzayche <dharm.serg@gmail.com>
2023-06-07 12:51:14 +0300

    Adding streams list

--- src/components/PlayerContainer.js
+++ src/components/PlayerContainer.js
@@ -2,7 +2,15 @@ import './PlayerContainer.scss';
 import ControlButton, { controlTypes } from "./ControlButton";
 import VolumeBar from "./Volume";
 import Button from "./Button";
+import StreamsList from "./StreamsList";
+import { useState } from "react";
 const PlayerContainer = () => {
+  const [streamsListCollapsed, setStreamsListCollapsed] = useState(false);
+  const toggleStreamsList = (e) => {
+    e.preventDefault();
+    setStreamsListCollapsed(!streamsListCollapsed);
+  }
+
   return (
     <div className="Player">
       <div className='Player__controlsContainerMain'>
@@ -23,7 +31,9 @@ const PlayerContainer = () => {
         </div>
 
       </div>
-      <Button className='Player__showStreamsLink' href='' >Show available streams -&gt;</Button>
+      <Button onClick={toggleStreamsList} className='Player__showStreamsLink' href='' >{streamsListCollapsed ? 'Hide available streams <-' : 'Show available streams ->' }</Button>
+
+      <StreamsList collapsed={streamsListCollapsed} className='Player_streamList' />
     </div>
   );
 };
