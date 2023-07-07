71a61f42943a0c2fa3cb0b62cdba5b9554c09541
banzayche <dharm.serg@gmail.com>
2023-07-07 15:50:36 +0300

    Logic refactoring

--- src/components/PlayerContainer.js
+++ src/components/PlayerContainer.js
@@ -1,4 +1,4 @@
-import './PlayerContainer.scss';
+import "./PlayerContainer.scss";
 import ControlButton, { controlTypes } from "./ControlButton";
 import VolumeBar from "./Volume";
 import Button from "./Button";
@@ -8,137 +8,198 @@ import useStore from "../store/PlayerStore";
 import { shallow } from "zustand/shallow";
 import { v4 as uuid } from "uuid";
 
-const mockedStreams = [{
-  name: 'Buffalo',
-  src: 'http://stream-uk1.radioparadise.com/mp3-192',
-  id: uuid()
-}, {
-  name: 'Radio z Kryjivky',
-  src: 'http://stream.mjoy.ua:8000/radio-z-kryjivky',
-  id: uuid()
-}, {
-  name: 'Amsterdam Trance',
-  src: 'http://sc-atr.1.fm:7700/;stream.nsv',
-  id: uuid()
-}, {
-  name: 'MFM Station',
-  src: 'http://radio.mfm.ua:8080/online128',
-  id: uuid()
-}];
+const mockedStreams = [
+  {
+    name: "Buffalo",
+    src: "http://stream-uk1.radioparadise.com/mp3-192",
+    id: uuid(),
+  },
+  {
+    name: "Radio z Kryjivky",
+    src: "http://stream.mjoy.ua:8000/radio-z-kryjivky",
+    id: uuid(),
+  },
+  {
+    name: "Amsterdam Trance",
+    src: "http://sc-atr.1.fm:7700/;stream.nsv",
+    id: uuid(),
+  },
+  {
+    name: "MFM Station",
+    src: "http://radio.mfm.ua:8080/online128",
+    id: uuid(),
+  },
+];
 
 const PlayerContainer = () => {
+  const defaultVolume = 0.3;
+  const volumeCoef = 0.01;
+
   const [streamsListCollapsed, setStreamsListCollapsed] = useState(false);
-  const [volume, setVolume] = useState(0.3);
-  const [volumeSnapshot, setVolumeSnapshot] = useState(0.05);
-  const [isPlay, setIsPlay] = useState(false);
+  const [volume, setVolume] = useState(defaultVolume);
+  const [volumeSnapshot, setVolumeSnapshot] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
   const [activeStation, setActiveStation, setStations, stationsList] = useStore(
-    (state) => [state.activeStation, state.setActiveStation, state.setStations, state.stationsList],
+    (state) => [
+      state.activeStation,
+      state.setActiveStation,
+      state.setStations,
+      state.stationsList,
+    ],
     shallow
   );
   const audio = useRef(new Audio());
 
   useEffect(() => {
     setStations(mockedStreams);
-    const listener = () => setIsPlay(true);
-    const pauseListener = () => setIsPlay(false);
-    audio.current.addEventListener('canplay', listener);
-    audio.current.addEventListener("pause", pauseListener);
-    audio.current.addEventListener("play", listener);
+    const listener = () => {
+      setIsLoading(false);
+      setIsPlaying(true);
+    };
+    audio.current.addEventListener("canplay", listener);
 
     return () => {
-      audio.current.removeEventListener('canplay', listener);
-      audio.current.removeEventListener('pause', pauseListener);
-      audio.current.removeEventListener('play', listener);
-    }
+      audio.current.removeEventListener("canplay", listener);
+    };
   }, []);
 
   useEffect(() => {
     if (activeStation.src) {
-      setIsPlay(false);
+      setIsLoading(true);
+      setIsPlaying(false);
       audio.current.src = activeStation.src;
     }
   }, [activeStation]);
 
   useEffect(() => {
-    if (volume > 0.05) {
+    if (volume > volumeCoef) {
       setVolumeSnapshot(0);
       audio.current.volume = volume;
     } else {
-      audio.current.volume = volume;
+      audio.current.volume = 0;
     }
   }, [volume]);
 
   useEffect(() => {
     if (audio.current.src) {
-      if (isPlay) audio.current.play();
+      if (isPlaying) audio.current.play();
       else audio.current.pause();
     }
-  }, [isPlay]);
+  }, [isPlaying]);
 
   const toggleStreamsList = (e) => {
     e.preventDefault();
     setStreamsListCollapsed(!streamsListCollapsed);
-  }
+  };
 
   const onStationSelect = function (station, e) {
     e.preventDefault();
     setActiveStation(station);
-  }
+  };
 
   const onVolumeChange = (event) => {
     const newVolume = Number(event.target.value);
     setVolume(newVolume);
   };
 
-  const playPause = () => setIsPlay(!isPlay)
+  const playPause = () => setIsPlaying(!isPlaying);
 
   const onMute = () => {
-    if (volumeSnapshot <= 0.05) {
-      setVolumeSnapshot(volume);
-      setVolume(0.05)
-    } else {
+    if (volumeSnapshot) {
       setVolume(volumeSnapshot);
-      setVolumeSnapshot(0.05);
+      setVolumeSnapshot(0);
+    } else {
+      setVolumeSnapshot(volume);
+      setVolume(0);
     }
-  }
+  };
 
   const setPrevStation = () => {
-    let index = stationsList.findIndex(station => station.id === activeStation.id) - 1;
+    let index =
+      stationsList.findIndex((station) => station.id === activeStation.id) - 1;
     if (index === -2) return;
     if (index === -1) index = stationsList.length - 1;
     setActiveStation(stationsList[index]);
-  }
+  };
 
   const setNextStation = () => {
-    let index = stationsList.findIndex(station => station.id === activeStation.id) + 1;
+    let index =
+      stationsList.findIndex((station) => station.id === activeStation.id) + 1;
     if (index === 0) return;
     if (index === stationsList.length) index = 0;
     setActiveStation(stationsList[index]);
-  }
+  };
 
   return (
     <div className="Player">
-      <div className='Player__controlsContainerMain'>
-        <ControlButton disabled={!activeStation.src} onClick={playPause} title={isPlay ? 'Play' : 'Pause'} controlType={isPlay ? controlTypes.pause : controlTypes.play} />
+      <div className="Player__controlsContainerMain">
+        <ControlButton
+          disabled={!activeStation.src || isLoading}
+          onClick={playPause}
+          title={isPlaying ? "Pause" : "Play"}
+          controlType={isPlaying ? controlTypes.pause : controlTypes.play}
+        />
 
-        <div className='controlsContainerMain__controlsContainerInner'>
-          <div>{activeStation.name || 'Chose radio station...'}</div>
-          <div className='controlsContainerInner__nextPrevVolumeControlsContainer'>
-            <div className='nextPrevVolumeControlsContainer__prevNextContainer'>
-              <ControlButton disabled={!activeStation.src} onClick={setPrevStation} title='Previous' controlType={controlTypes.prev} />
-              <ControlButton disabled={!activeStation.src} onClick={setNextStation} title='Next' controlType={controlTypes.next} />
+        <div className="controlsContainerMain__controlsContainerInner">
+          <div>
+            {isLoading
+              ? "...Loading"
+              : isPlaying
+              ? activeStation.name
+              : "Chose radio station..."}
+          </div>
+          <div className="controlsContainerInner__nextPrevVolumeControlsContainer">
+            <div className="nextPrevVolumeControlsContainer__prevNextContainer">
+              <ControlButton
+                disabled={!activeStation.src || isLoading}
+                onClick={setPrevStation}
+                title="Previous"
+                controlType={controlTypes.prev}
+              />
+              <ControlButton
+                disabled={!activeStation.src || isLoading}
+                onClick={setNextStation}
+                title="Next"
+                controlType={controlTypes.next}
+              />
             </div>
-            <div className='nextPrevVolumeControlsContainer__volumeContainer'>
-              <ControlButton  onClick={onMute} title='Volume' controlType={volume <= 0.05 ? controlTypes.volumeOff : controlTypes.volumeOn} />
-              <VolumeBar onVolumeChange={onVolumeChange} volumeValue={volume}></VolumeBar>
+            <div className="nextPrevVolumeControlsContainer__volumeContainer">
+              <ControlButton
+                onClick={onMute}
+                title="Volume"
+                controlType={
+                  volume <= volumeCoef
+                    ? controlTypes.volumeOff
+                    : controlTypes.volumeOn
+                }
+              />
+              <VolumeBar
+                onVolumeChange={onVolumeChange}
+                volumeValue={volume}
+              ></VolumeBar>
             </div>
           </div>
         </div>
-
       </div>
-      <Button onClick={toggleStreamsList} className='Player__showStreamsLink' href='' >{streamsListCollapsed ? 'Hide available streams <-' : 'Show available streams ->' }</Button>
+      <Button
+        onClick={toggleStreamsList}
+        className="Player__showStreamsLink"
+        href=""
+      >
+        {streamsListCollapsed
+          ? "Hide available streams <-"
+          : "Show available streams ->"}
+      </Button>
 
-      <StreamsList activeStation={activeStation} selectStream={onStationSelect} streams={stationsList} collapsed={streamsListCollapsed} className='Player_streamList' />
+      <StreamsList
+        activeStation={activeStation}
+        selectStream={onStationSelect}
+        streams={stationsList}
+        collapsed={streamsListCollapsed}
+        className="Player_streamList"
+      />
     </div>
   );
 };
--- src/components/Volume.js
+++ src/components/Volume.js
@@ -14,7 +14,7 @@ const VolumeBar = memo(({volumeValue, className, onVolumeChange, ...props}) => {
         type="range"
         min={0}
         max={1}
-        step='0.05'
+        step='0.01'
         value={volumeValue}
         onChange={handleVolumeChange}
       />
