86f15cf8813b1818fd446dea7aef29914ae9ed19
banzayche <dharm.serg@gmail.com>
2023-07-09 13:29:35 +0300

    App as styled components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,84 @@
-import './App.scss';
-import ThemeContext, { colorThemeSchema } from "./contexts/ThemeContext";
 import { useEffect, useState } from "react";
 import classNames from "classnames";
 import Toggle from "./components/ui-kit/Toggle";
+import styled from 'styled-components';
 import { Link, Outlet, useLocation } from "react-router-dom";
+import ThemeContext, { colorThemeSchema } from "./contexts/ThemeContext";
+import { themeMixin } from "./index";
 
+const StyledApp = styled.div`
+  ${themeMixin};
+  height: 100%;
+  transition-property: background-color, color;
+  transition-timing-function: linear;
+  transition-duration: .5s;
+
+  &.dark {
+    background-color: var(--dark-mode-bg);
+    color: var(--dark-mode-text);
+    border-color: var(--dark-mode-text);
+  }
+  &.light {
+    background-color: var(--ligh-mode-bg);
+    color: var(--light-mode-text);
+    border-color: var(--light-mode-text);
+  }
+
+  .top-bar {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    padding: 20px 40px;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  opacity: 0.8;
+  font-size: 12px;
+  font-style: oblique;
+  display: inline-block;
+  letter-spacing: .15rem;
+  transition: var(--transition);
+  padding: 0;
+  text-underline-offset: 5px;
+  font-weight: 400;
+  color: inherit;
+
+  &:active {
+    transform: scale(.9,.9);
+  }
+
+  &:hover {
+    opacity: 1;
+  }
+`;
 
 const App = () => {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState('');
   const {pathname} = useLocation();
   useEffect(() => {
-    const colorTheme = JSON.parse(localStorage.getItem('theme')) || 'dark';
-    updateTheme(colorTheme);
+    const colorTheme = localStorage.getItem('theme') || 'dark';
+    setTheme(colorTheme);
   }, []);
 
-  const updateTheme = (newValue) => {
-    setTheme(newValue);
-  };
+  useEffect(() => {
+    if (theme) localStorage.setItem('theme', theme);
+  }, [theme]);
 
   return (
-    <ThemeContext.Provider value={{theme, updateTheme}}>
+    <ThemeContext.Provider value={{theme, setTheme}}>
 
-      <div className={classNames('App', {dark: theme === colorThemeSchema.dark, light: theme === colorThemeSchema.light})}>
-        <div className='theme-toggle'>
-          <Link to={pathname === '/discovery' ? '/' : '/discovery'} className="__goToLink">{pathname === '/discovery' ? '<---' : 'Components sets page'}</Link>
+      <StyledApp className={classNames({dark: theme === colorThemeSchema.dark, light: theme === colorThemeSchema.light})}>
+        <div className='top-bar'>
+          <StyledLink to={pathname === '/discovery' ? '/' : '/discovery'}>{pathname === '/discovery' ? '<---' : 'Components sets page'}</StyledLink>
           <label>
             <Toggle checked={theme === colorThemeSchema.dark} value={theme} onChange={() => setTheme(theme === colorThemeSchema.dark ? colorThemeSchema.light : colorThemeSchema.dark)} />
-            <span className="toggle-label">Switch to {theme === colorThemeSchema.dark ? colorThemeSchema.light : colorThemeSchema.dark} theme</span>
+            <span>Switch to {theme === colorThemeSchema.dark ? colorThemeSchema.light : colorThemeSchema.dark} theme</span>
           </label>
         </div>
         <Outlet></Outlet>
-      </div>
+      </StyledApp>
     </ThemeContext.Provider>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import App from "./App.js";
 import { createHashRouter, RouterProvider } from "react-router-dom";
 import PlayerContainer from "./pages/PlayerContainer";
 import Discovery from "./pages/Discovery";
+import "./reset.css";
+import { css } from "styled-components";
+
+export const themeMixin = css`
+  transition-property: background-color, color;
+  transition-timing-function: linear;
+  transition-duration: .5s;
+
+  &.dark {
+    background-color: var(--dark-mode-bg);
+    color: var(--dark-mode-text);
+    border-color: var(--dark-mode-text);
+  }
+  &.light {
+    background-color: var(--ligh-mode-bg);
+    color: var(--light-mode-text);
+    border-color: var(--light-mode-text);
+  }
+`;
 
 const router = createHashRouter([
   {
@@ -20,5 +39,5 @@ const router = createHashRouter([
   },
 ]);
 
-let root = createRoot( document.getElementById('root'));
+let root = createRoot(document.getElementById('root'));
 root.render(<RouterProvider router={router} />);
